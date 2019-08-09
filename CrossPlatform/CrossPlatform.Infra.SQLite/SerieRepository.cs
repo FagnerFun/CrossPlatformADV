b00f106d8172bcc477b98b48fa0a4de07ed33820
Fagner Muniz <f.santos@cinq.com.br>
2019-08-09 11:37:52 -0300

    SQLite - update serie repository constructor

--- CrossPlatform/CrossPlatform.Infra.SQLite/SerieRepository.cs
+++ CrossPlatform/CrossPlatform.Infra.SQLite/SerieRepository.cs
@@ -1,9 +1,13 @@
 ﻿using CrossPlatform.Domain.Interface.Repository;
+using CrossPlatform.Domain.Interface.Service;
 using CrossPlatform.Domain.Model;
 
 namespace CrossPlatform.Infra.SQLite
 {
-    public class SerieRepository: Repository<Serie>, ISerieRepository
+    public class SerieRepository : Repository<Serie>, ISerieRepository
     {
+        public SerieRepository(IDataBaseAccessService dataBaseAccessService) : base(dataBaseAccessService)
+        {
+        }
     }
 }

2a3a466838a5e2501b804d6277a296cc1de9740a
Fagner Muniz <f.santos@cinq.com.br>
2019-08-09 11:26:23 -0300

    SQLite update Pictures resize

--- CrossPlatform/CrossPlatform.Infra.SQLite/Repository.cs
+++ CrossPlatform/CrossPlatform.Infra.SQLite/Repository.cs
@@ -1,30 +1,60 @@
 ﻿using CrossPlatform.Domain.Interface.Repository;
+using CrossPlatform.Domain.Interface.Service;
 using CrossPlatform.Domain.Model;
+using SQLite;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CrossPlatform.Infra.SQLite
 {
-    public class Repository<T> : IRepository<T> where T : DBEntity
+    public class Repository<T> : SQLiteConnection, IDisposable, IRepository<T> where T : DBEntity, new()
     {
+        private readonly IDataBaseAccessService dataBaseAccessService;
+
+        public Repository(IDataBaseAccessService dataBaseAccessService): base(dataBaseAccessService.GetDataBasePath())
+        {
+            this.dataBaseAccessService = dataBaseAccessService;
+
+            CreateTable<T>();
+        }
+
         public void Add(T entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                BeginTransaction();
+                Insert(entity);
+                Commit();
+            }
+            catch
+            {
+                Rollback();
+            }
         }
 
         public void Delete(T entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                BeginTransaction();
+                Delete(entity);
+                Commit();
+            }
+            catch
+            {
+                Rollback();
+            }
         }
 
         public bool Exist(T Entity)
         {
-            throw new NotImplementedException();
+            return (from c in Table<T>() where c.Id == Entity.Id select c).Any();
         }
 
         public IEnumerable<T> GetAll()
         {
-            throw new NotImplementedException();
+            return (from c in Table<T>() select c);
         }
     }
 }
